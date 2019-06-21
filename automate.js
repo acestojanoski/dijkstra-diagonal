c7824389cc6cc125fe6444a656e62b64661bf613
Aleksandar Stojanoski <aleksandar-stojanoski@hotmail.com>
2019-06-21 23:28:15 +0200

    automate

---
+++ automate.js
@@ -0,0 +1,22 @@
+const handleResult = (result) => {
+  let resultName = 'dijkstraResult';
+  let previousResult = localStorage.getItem(resultName);
+  if (previousResult) {
+    localStorage.setItem(resultName, `${localStorage.getItem(resultName)}, ${result}`);
+  }
+}
+
+let observer = new MutationObserver((mutations, me) => {
+  let result = document.getElementById('title').innerText;
+  if (result.indexOf('took') !== -1) {
+    handleResult(result);
+    me.disconnect(); // stop observing
+    location.reload();
+  }
+});
+
+// start observing
+observer.observe(document, {
+  childList: true,
+  subtree: true
+});
